40c6c465b32ba22ff4c86f4ed35eab3760d0d705
davidroman-hub <jobroman83@gmail.com>
2020-06-18 23:13:17 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+//Pages
+import ContactMe from './pages/ContactMe/ContactMe'
+import NewHome from './pages/NewHome/Home'
+//import FooterV from './pages/ContactMe/Footer/Footer'
+// import AboutMe from './pages/AboutMe/AboutMe'
+/// Navbar Components ///
+
+import Backdrop from './shared/Backdrop/Backdrop'
+import SideDrawer from './shared/navbar/Sidredrawer/Sidedrawer'
+import Navbar from './shared/navbar/navbar'
+import './App.css';
+
+class App extends Component{
 
 
-function App() {
-  return (
-    <div className="App">
-  <h1> im testing netli</h1>
+  //  State  //
+  state = { 
+    sideDrawerOpen:false 
+  }
 
-    </div>
-  );
+  // the function //
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {return{
+      sideDrawerOpen: !prevState.sideDrawerOpen 
+    } });
+  } ;
+
+  backdropClickHandker = () => {
+    this.setState({ sideDrawerOpen:false })
+  }
+
+render(){
+
+  let backdrop;
+  if( this.state.sideDrawerOpen){
+    backdrop = <Backdrop click = {this.drawerToggleClickHandler}/>
+  }
+
+
+  return(
+    <BrowserRouter>
+      <Fragment>
+        <Navbar drawerClickHandler = {this.drawerToggleClickHandler}/>
+        <SideDrawer show = {this.state.sideDrawerOpen}/>
+        {backdrop}
+        <Route exact path='/' component={NewHome}></Route>
+        {/* <Route exact path='/ContactMe' component={ContactMe}></Route> */}
+        <Route exact path='/home' component={NewHome}></Route>
+        {/* <Route exact path='/AboutMe' component={AboutMe}></Route> */}
+
+      {/* <FooterV></FooterV> */}
+      </Fragment>
+    </BrowserRouter>
+  )
+}
 }
 
+
+
 export default App;
+
